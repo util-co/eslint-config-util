0a01aafc5433f43b4955d9bca25026f55a80ff17
Shivam Mehta <mehtashivam91@gmail.com>
2023-01-18 10:55:26 +0000

    fix: string to boolean

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "no-empty-pattern": "off",
     "@typescript-eslint/no-empty-interface": "off",
-    "react/no-multi-comp": ['error', { "ignoreStateless": 'true' }],
+    "react/no-multi-comp": ['error', { "ignoreStateless": true }],
     "max-lines-per-function": ["error", 200],
     "react-func/max-lines-per-function": [
       "error",
